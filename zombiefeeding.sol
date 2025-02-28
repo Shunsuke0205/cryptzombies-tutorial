e700243308fb22fdb54036e850b2955fd5ebaafb
Shunsuke0205 <hirata-shunsuke1125@g.ecc.u-tokyo.ac.jp>
2025-02-28 11:00:11 +0900

    2-12: Handling Multiple Return Values

--- zombiefeeding.sol
+++ zombiefeeding.sol
@@ -28,6 +28,11 @@ contract ZombieFeeding is ZombieFactory {
         _targetDna = _targetDna % dnaModulus;
         uint newDna = (myZombie.dna + _targetDna) / 2;
         _createZombie("NoName", newDna);
+    }
 
+    function feedOnKitty(uint _zombieId, uint _kittyId) public {
+        uint kittyDna;
+        (,,,,,,,,,kittyDna) = kittyContract.getKitty(_kittyId);
+        feedAndMultiply(_zombieId, kittyDna);
     }
 }
