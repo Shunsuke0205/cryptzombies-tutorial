c30d8df2c0e65e1f6d846fb4269aeffc40a36d62
Shunsuke0205 <hirata-shunsuke1125@g.ecc.u-tokyo.ac.jp>
2025-03-07 09:38:35 +0900

    5-13: Comments. FINISH lesson 5!

--- zombieownership.sol
+++ zombieownership.sol
@@ -3,6 +3,9 @@ pragma solidity >=0.5.0 <0.6.0;
 import "./zombieattack.sol";
 import "./erc721.sol";
 
+/// @title A contract that manages transferring zombie ownership
+/// @author Shunsuke HIRATA
+/// @dev Compliant with OpenZeppelin's implementation of the ERC721 spec draft
 contract ZombieOwnership is ZombieAttack, ERC721 {
 
     mapping (uint => address) zombieApprovals;
